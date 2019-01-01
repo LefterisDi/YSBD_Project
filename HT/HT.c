2d4da35132990ad04ad2051afb4d71ecd6706346
root <xan.michael98@yahoo.com>
2019-01-01 19:10:32 +0200

    commit

--- HT/HT.c
+++ HT/HT.c
@@ -1,3 +1,33 @@
 /* File: Ht.c */
 
 #include "HT.h"
+
+int HT_CreateIndex(char* fileName, char  attrType, char* attrName, int   attrLength, int buckets)
+{
+
+}
+
+HT_info* HT_OpenIndex(char* fileName)
+{
+
+}
+
+int HT_CloseIndex(HT_info* header_info)
+{
+
+}
+
+int HT_InsertEntry(HT_info header_info, Record record)
+{
+
+}
+
+int HT_DeleteEntry(HT_info header_info, void* value)
+{
+
+}
+
+int HT_GetAllEntries( HT_info header_info, void* value)
+{
+
+}
